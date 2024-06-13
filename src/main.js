5926ea77610a78f55916957ae39807f8246eed93
Vita Giricheva <nikaangel295@gmail.com>
2024-06-13 11:09:58 +0100

    add

--- src/main.js
+++ src/main.js
@@ -7,3 +7,42 @@ const form = document.querySelector('form');
 const input = document.querySelector('input[data-search]');
 const loader = document.querySelector('.loader-div');
 const list = document.querySelector('.list');
+
+form.addEventListener('submit', e => {
+e.preventDefault();
+loader.style.visibility = 'visible';
+const search = input.value.trim();
+list.innerHTML = '';
+pixApi(search)
+.then(data => {
+const result = data.hits;
+  
+if (result.length !== 0 && search !== '') {
+renderImages(result, list);
+} else {
+iziToast.show({
+title: '',
+message:
+'Sorry, there are no images matching your search query. Please try again!',
+messageColor: 'white',
+backgroundColor: '#E25757',
+position: 'topRight',
+ });
+}
+})
+.catch(error => {
+ console.error('Помилка при рендері картинок', error);
+iziToast.show({
+title: '',
+message: 'Sorry, check your internet connection!',
+messageColor: 'white',
+backgroundColor: '#E25757',
+position: 'topRight',
+timeout: 5000,
+});
+})
+.finally(() => {
+loader.style.visibility = 'hidden';
+e.target.reset();
+});
+});
