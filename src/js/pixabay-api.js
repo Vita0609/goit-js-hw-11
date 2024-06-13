82bbe87e0de6d080199516c1e8bd5b0477ccb160
Vita Giricheva <nikaangel295@gmail.com>
2024-06-13 11:03:54 +0100

    add

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+const apiKey = '44362939-998edb8f92844eab0dd18e81c';
+
+const pixApi = data => {
+  const searchParams = new URLSearchParams({
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  return fetch(
+    `https://pixabay.com/api/?key=${apiKey}&q=${data}&${searchParams}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Помилка при отриманні запиту');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error('Помилка при отриманні даних', error);
+      throw error;
+    });
+};
+
+export default pixApi;
